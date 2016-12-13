01ae88e7fa9a6478576a6b40d1e29dda3bda907f
unknown <rosid.abdul12@gmail.com>
2016-12-13 14:32:49 +0700

    grunt2

--- gruntfile.js
+++ gruntfile.js
@@ -156,7 +156,7 @@ module.exports = function(grunt) {
 
 	// Default task(s).
 	grunt.registerTask('default', ['lint', 'concurrent:default']);
-	//grunt.registerTask('default', 'exec copy-require');
+	//grunt.registerTask('default', ['exec','copy-require']);
 
 	// Debug task.
 	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
